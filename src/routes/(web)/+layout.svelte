fba5cfc05ce9139868c6f7f3b274d41bcceaa9f6
pysdenis <d.pys.st@spseiostrava.cz>
2024-05-15 11:14:17 +0200

    chore: Update logo and video elements in web layout

--- src/routes/(web)/+layout.svelte
+++ src/routes/(web)/+layout.svelte
@@ -6,16 +6,18 @@
 
 <header>
 	<div class="absolute z-50 w-full flex items-center gap-3 justify-center py-3 bg-transparent">
-		<img src={logo} alt="Logo" class="h-14" />
+		<a href="/">
+			<img src={logo} alt="Logo of the corporation" class="h-14" />
+		</a>
 	</div>
 	<div id="hero" class="relative">
-		<div class="absolute h-[100dvh] w-full overflow-hidden max-lg:mt-20">
+		<div class="absolute h-[100dvh] w-full overflow-hidden">
 			<video
 				autoplay
 				loop
 				muted
 				playsinline
-				class="absolute inset-0 h-screen max-h-[750px] w-screen object-cover"
+				class="absolute inset-0 h-screen max-h-[100dvh] w-screen object-cover"
 			>
 				<source src={heroVideo} type="video/mp4" />
 			</video>
@@ -29,7 +31,7 @@
 		</div>
 	</div>
 </header>
-<main class="container">
+<main class="container mt-[100dvh]">
 	<slot />
 </main>
 <footer class="flex items-center justify-center bg-secondary">
