85b41490efaf193d7bff0b7d1868d31c6380c304
pysdenis <d.pys.st@spseiostrava.cz>
2024-05-17 10:00:03 +0200

    Update web layout with improved logo and video elements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import logo from '$lib/assets/images/logo.svg';
 	import globo from '$lib/assets/images/globo.svg';
 	import heroVideo from '$lib/assets/video/hero.mp4';
+    import StaticPicture from '$lib/components/Picture/StaticPicture.svelte';
 
 	let windowScrollY = 0;
 </script>
@@ -12,7 +13,7 @@
 <header>
 	<div class:shadow-sm={windowScrollY > 40} class:!bg-black={windowScrollY > 0} class:!p-0={windowScrollY > 0} class="fixed transition-all duration-300 left-0 right-0 top-0 pb-3 pt-3 md:pb-14 md:pt-6 xl:pb-6 z-50 w-full flex items-center gap-3 justify-center py-3 bg-transparent">
 		<a href="/">
-			<img src={logo} alt="Logo of the corporation" class="h-14" />
+			<StaticPicture width={238} height={26} image={logo} loading="eager" alt="Logo of the corporation" class="h-14" />
 		</a>
 	</div>
 	<div id="hero" class="relative">
